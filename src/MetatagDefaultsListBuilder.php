fe6affa960e80d4a64fea184d06a874b2e64de01
Juampy <juampy72@gmail.com>
2015-11-24 15:13:33 +0100

    [#3] Show a summary of each defaults at the admin view

--- src/MetatagDefaultsListBuilder.php
+++ src/MetatagDefaultsListBuilder.php
@@ -26,9 +26,49 @@ class MetatagDefaultsListBuilder extends ConfigEntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
-    $row['label'] = $this->getLabel($entity);
-    // You probably want a few more properties here...
+    $row['label'] = $this->getLabelAndConfig($entity);
     return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * Renders the Metatag defaults lable plus its configuration.
+   *
+   * @param EntityInterface $entity
+   *   The Metatag defaults entity.
+   * @return
+   *   Render array for a table cell.
+   */
+  public function getLabelAndConfig(EntityInterface $entity) {
+    $output = '<div>';
+    $prefix = '';
+    if ($entity->id() != 'global') {
+      $output .= '<div>
+                   <p>Inherits meta tags from: Global</p>
+                 </div>';
+      $prefix = '<div class="indentation"></div>';
+    }
+    $tags = $entity->get('tags');
+    if (count($tags)) {
+      $output .= '<table>
+                    <tbody>';
+      foreach ($tags as $tag_id => $tag_value) {
+        $output .= '<tr><td>' . $tag_id . ':</td><td>' . $tag_value . '</td></tr>';
+      }
+      $output .= '</tbody></table>';
+    }
+
+    $output .= '</div></div>';
+
+    return array(
+      'data' => array(
+        '#type' => 'details',
+        '#prefix' => $prefix,
+        '#title' => $this->getLabel($entity),
+        'config' => array(
+          '#markup' => $output,
+        ),
+      ),
+    );
   }
 
 }

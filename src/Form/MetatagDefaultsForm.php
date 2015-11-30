470746e97ef08826636744aebebfd2a59d07dd24
Juampy <juampy72@gmail.com>
2015-11-30 11:01:06 +0100

    [#15] Remove add and delete operations plus bundle defaults
    Field API will take care of them.

--- src/Form/MetatagDefaultsForm.php
+++ src/Form/MetatagDefaultsForm.php
@@ -42,18 +42,6 @@ class MetatagDefaultsForm extends EntityForm {
       '#markup' => '<p>' . t('Configure the meta tags below. Use tokens (see the "Browse available tokens" popup) to avoid redundant meta data and search engine penalization. For example, a \'keyword\' value of "example" will be shown on all content using this configuration, whereas using the [node:field_keywords] automatically inserts the "keywords" values from the current entity (node, term, etc).') . '</p>',
     );
 
-    // If this is a new Metatag defaults, then list available bundles.
-    if ($metatag_defaults->isNew()) {
-      $options = $this->getAvailableBundles();
-      $form['id'] = array(
-        '#type' => 'select',
-        '#title' => t('Type'),
-        '#description' => t('Select the type of default meta tags you would like to add.'),
-        '#options' => $options,
-        '#required' => TRUE,
-      );
-    }
-
     // Load all tag plugins and render their form representation.
     $tag_manager = \Drupal::service('plugin.manager.metatag.tag');
     $tags = $tag_manager->getDefinitions();
@@ -75,21 +63,6 @@ class MetatagDefaultsForm extends EntityForm {
   public function save(array $form, FormStateInterface $form_state) {
     $metatag_defaults = $this->entity;
 
-    // Set the label on new defaults.
-    if ($metatag_defaults->isNew()) {
-      $metatag_defaults_id = $form_state->getValue('id');
-      list($entity_type, $entity_bundle) = explode('__', $metatag_defaults_id);
-      // Get the entity label.
-      $entity_manager = \Drupal::service('entity.manager');
-      $entity_info = $entity_manager->getDefinitions();
-      $entity_label = (string) $entity_info[$entity_type]->get('label');
-      // Get the bundle label.
-      $bundle_info = $entity_manager->getBundleInfo($entity_type);
-      $bundle_label = $bundle_info[$entity_bundle]['label'];
-      // Set the label to the config entity.
-      $this->entity->set('label', $entity_label . ': ' . $bundle_label);
-    }
-
     // Set tags within the Metatag entity.
     $tag_manager = \Drupal::service('plugin.manager.metatag.tag');
     $tags = $tag_manager->getDefinitions();
@@ -107,46 +80,11 @@ class MetatagDefaultsForm extends EntityForm {
     }
     $metatag_defaults->set('tags', $tag_values);
     $status = $metatag_defaults->save();
-
-    switch ($status) {
-      case SAVED_NEW:
-        drupal_set_message($this->t('Created the %label Metatag defaults.', [
-          '%label' => $metatag_defaults->label(),
-        ]));
-        break;
-      default:
-        drupal_set_message($this->t('Saved the %label Metatag defaults.', [
-          '%label' => $metatag_defaults->label(),
-        ]));
-    }
+    drupal_set_message($this->t('Saved the %label Metatag defaults.', [
+      '%label' => $metatag_defaults->label(),
+    ]));
 
     $form_state->setRedirectUrl($metatag_defaults->urlInfo('collection'));
-  }
-
-  /**
-   * Returns an array of available bundles to override.
-   *
-   * @return array
-   *   A list of available bundles as $id => $label.
-   */
-  protected function getAvailableBundles() {
-    $options = array();
-    // @TODO discover supported entities.
-    $entity_types = array(
-      'node' => 'Node',
-      'taxonomy_term' => 'Taxonomy term',
-    );
-    $entity_manager = \Drupal::service('entity.manager');
-    foreach ($entity_types as $entity_type => $entity_label) {
-      $bundles = $entity_manager->getBundleInfo($entity_type);
-      foreach ($bundles as $bundle_id => $bundle_metadata) {
-        $metatag_defaults_id = $entity_type . '__' . $bundle_id;
-        if (empty(entity_load('metatag_defaults', $metatag_defaults_id))) {
-          $options[$entity_label][$metatag_defaults_id] = $bundle_metadata['label'];
-        }
-      }
-    }
-    return $options;
   }
 
 }
